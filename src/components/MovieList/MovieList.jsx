f81573c64b124be55f9bf6e1e441913ab25f6f4a
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-12-16 14:37:43 +0200

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,26 +1,18 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MovieList = () => {
+export const MovieList = ({movies}) => {
   return (
     <div className="movies">
-      <MovieCard
-        description="Follows the lives of eight very different couples in
-          dealing with their love lives in various loosely interrelated tales
-           all set during a frantic month before Christmas in London, England."
-        image="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-        title="Inception"
-        link="https://www.imdb.com/title/tt1375666"
-      />
-      <MovieCard
-        description="A thief who steals corporate secrets through the use of
-              dream-sharing technology is given the inverse task of planting
-              an idea into the mind of a C.E.O."
-        image="https://m.media-amazon.com/images/M/MV5BMTY4NjQ5NDc0Nl5BMl5BanBnXkFtZTYwNjk5NDM3._V1_.jpg"
-         title="Love Actually"
-          link="https://www.imdb.com/title/tt0314331"
-      />
-
+      {movies.map(movie =>
+        <MovieCard
+          key = {movie.imdbId}
+          description = {movie.description}
+          title = {movie.title}
+          image = {movie.imgUrl}
+          link ={movie.imdbUrl}
+        />
+      )}
     </div>
   );
 }
